e70e3edf67a61aaed3d3db0902196c80ccdced55
Robrecht Cannoodt <rcannood@gmail.com>
2020-09-21 17:02:08 +0200

    another attempt

--- tests/testthat/helper-skip.R
+++ tests/testthat/helper-skip.R
@@ -3,5 +3,5 @@ skip_on_travis_mac <- function() {
   skip_if(Sys.getenv("TRAVIS") == "true" && "darwin" %in% tolower(Sys.info()[["sysname"]]))
 }
 skip_on_github_actions <- function() {
-  skip_if(Sys.getenv("GITHUB_ACTIONS_OS") == "MacOSX")
+  skip_if(Sys.getenv("GA_OS") == "MacOSX")
 }
