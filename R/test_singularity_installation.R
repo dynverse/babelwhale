5d5daafda4c6204c011929c0e8fad1d62c2809fb
Robrecht Cannoodt <rcannood@gmail.com>
2023-07-14 11:33:29 +0200

    don't do a version check on apptainer

--- R/test_singularity_installation.R
+++ R/test_singularity_installation.R
@@ -29,22 +29,30 @@ test_singularity_installation <- function(detailed = FALSE) {
 
     if (output$status == 1) {
       stop(crayon::red(paste0(
-        "\u274C Singularity (>= 3.0) could not be found. ",
+        "\u274C Singularity (>= 3.0) or Apptainer could not be found. ",
         "Install the latest stable release from: https://github.com/sylabs/singularity/releases."
       )))
     }
 
-    major_version <- as.integer(gsub("^([^.]*)\\..*", "\\1", output$stdout))
-
-    if (major_version < 3) {
-      stop(crayon::red(paste0(
-        "\u274C An older version of Singularity was detected (", output$stdout, "). ",
-        "Install the latest stable release following the installation instructions at https://sylabs.io/docs/."
-      )))
-    }
-
     message(crayon::green(paste0("\u2714 Singularity is installed")))
-    message(crayon::green(paste0("\u2714 Singularity is at correct version (>=3.0): ", gsub("\n", "", output$stdout), " is installed")))
+
+    output2 <- tryCatch(
+      processx::run("singularity", "--help", error_on_status = FALSE, stderr_callback = print_processx, timeout = 2),
+      error = function(e) {list(status = 1, message = e$message)}
+    )
+
+    if (!grepl("apptainer", output2$stdout))
+      major_version <- as.integer(gsub("^([^.]*)\\..*", "\\1", output$stdout))
+
+      if (major_version < 3) {
+        stop(crayon::red(paste0(
+          "\u274C An older version of Singularity was detected (", output$stdout, "). ",
+          "Install the latest stable release following the installation instructions at https://sylabs.io/docs/."
+        )))
+      }
+
+      message(crayon::green(paste0("\u2714 Singularity is at correct version (>=3.0): ", gsub("\n", "", output$stdout), " is installed")))
+    }
 
     output <- processx::run("singularity", c("exec", "docker://alpine:3.7", "echo", "hi"), error_on_status = FALSE, stderr_callback = print_processx, spinner = TRUE, echo = FALSE)
     if (output$status != 0 || output$stdout != "hi\n") {
