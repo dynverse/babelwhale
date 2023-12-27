8361dc6e6ff353298be6dc8ebe162a0779bffc6e
szcf-weiya <szcfweiya@gmail.com>
2023-12-27 17:49:52 -0500

    pull sif image if not exists

--- R/pull_image.R
+++ R/pull_image.R
@@ -14,7 +14,11 @@ pull_container <- function(container_id) {
     processx::run("docker", c("pull", container_id), echo = TRUE)
 
   } else if (config$backend == "singularity") {
-    processx::run("singularity", c("exec", paste0("docker://", container_id), "echo", "hi"), echo_cmd = TRUE, echo = FALSE)
+    local_sif_path <- Sys.getenv("LOCAL_SIF_PATH")
+    container_sif <- paste0(local_sif_path, "/", gsub("[/,:]", "_", container_id), ".sif")
+    if (!file.exists(container_sif)) {
+      processx::run("singularity", c("pull", container_sif, paste0("docker://", container_id)), echo_cmd = TRUE, echo = FALSE)  
+    }
   }
 
   return(TRUE)
